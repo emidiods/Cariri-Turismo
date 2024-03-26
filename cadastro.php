f46fd7cdd67b776792021bc4bb9afc3b85e5685d
emidiods <160769710+emidiods@users.noreply.github.com>
2024-03-26 11:58:17 -0300

    cariri_turismo
    gg

---
+++ cadastro.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="css/boosttrap.min.css">
+</head>
+<body>
+<div class="container">
+<div class="linha">
+<div class="coluna">
+    <h1>Cadastre-se</h1>
+    <form>
+
+    <div class= "mb-3">
+    <label for="nome" class="form-label">Nome Completo</label>
+    <input type="text" class="form-control" name="nome">  
+    <br><br>
+    <div class= "mb-3">
+    <label for="telefone" class="form-label">telefone</label>
+    <input type="tel" class="form-control" name="telefone">   
+    <br><br>
+    <div class= "mb-3">
+    <label for="data" class="form-label">Dta de nascimento</label>
+    <input type="date" class="form-control" name="data"> 
+    <br><br>
+    <div class= "mb-3">
+    <label for="cpf" class="form-label">Cpf</label>
+    <input type="number" class="form-control" name="nome">   
+    <br><br>
+    <div class= "mb-3">
+    <label for="email" class="form-label">Email</label>
+    <input type="email" class="form-control" name="email">   
+    <br><br>
+    <div class= "mb-3">
+    <label for="senha" class="form-label">Senha</label>
+    <input type="Password" class="form-control" name="senha">   
+    <input type="submit" class="btn btn-sucess">
+    <br><br>
+</form>
+</div>
+</div>
+</div>
+</body>
+</html>
